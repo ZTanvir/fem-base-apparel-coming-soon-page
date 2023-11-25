070aeda21a1e04ba86c5bc358844ed122fff9a2a
ztanvir <zahirulopel@gmail.com>
2023-11-25 20:40:27 +0600

    Component subtitle explain the description about this page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Logo from "./Component/Logo";
 import Title from "./Component/Title";
+import Subtitle from "./Component/Subtitle";
 
 function App() {
   return (
     <>
       <Logo />
       <Title />
+      <Subtitle />
     </>
   );
 }
---
+++ src/Component/Subtitle.jsx
@@ -0,0 +1,10 @@
+const Subtitle = () => {
+  return (
+    <p>
+      Hello fellow shoppers! We're currently building our new fashion store. Add
+      your email below to stay up-to-date with announcements and our launch
+      deals.
+    </p>
+  );
+};
+export default Subtitle;
