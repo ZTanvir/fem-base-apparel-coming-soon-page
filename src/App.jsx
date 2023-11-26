1a8009d2000bf9c2d3834e62cd1cc43bc134da91
ztanvir <zahirulopel@gmail.com>
2023-11-26 20:16:26 +0600

    Hero image component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import Logo from "./Component/Logo";
 import Title from "./Component/Title";
 import Subtitle from "./Component/Subtitle";
 import GetNotifiedForm from "./Component/GetNotifiedForm";
+import HeroImage from "./Component/HeroImage";
 
 function App() {
   return (
-    <>
+    <div className="wrapper">
       <Logo />
       <Title />
       <Subtitle />
       <GetNotifiedForm />
-    </>
+      <HeroImage />
+    </div>
   );
 }
 
---
+++ src/Component/HeroImage.jsx
@@ -0,0 +1,6 @@
+import heroImageDesktop from "../assets/images/hero-desktop.jpg";
+import heroImageMobile from "../assets/images/hero-mobile.jpg";
+const HeroImage = () => {
+  return <img src={heroImageDesktop} alt="A girl wearing orange shirt" />;
+};
+export default HeroImage;
