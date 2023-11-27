8ec514052bdfb7ce636295f26d30639d043e813a
ztanvir <zahirulopel@gmail.com>
2023-11-27 20:53:43 +0600

    style for title section will be added

--- src/Component/Title.jsx
+++ src/Component/Title.jsx
@@ -1,7 +1,9 @@
+import titleStyle from "../assets/Styles/title.module.css";
+
 const Title = () => {
   return (
-    <h1>
-      <span className="color-text">WE'RE</span>
+    <h1 className={titleStyle.title}>
+      <span className={titleStyle.highlightTitle}>WE'RE</span>
       <span>COMING</span>
       <span>SOON</span>
     </h1>
