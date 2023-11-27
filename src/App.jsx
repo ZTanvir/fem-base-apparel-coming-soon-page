16e1408c7adceca9c38a9df6ddd804c478207c11
ztanvir <zahirulopel@gmail.com>
2023-11-27 20:30:04 +0600

    app page css added

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import Title from "./Component/Title";
 import Subtitle from "./Component/Subtitle";
 import GetNotifiedForm from "./Component/GetNotifiedForm";
 import HeroImage from "./Component/HeroImage";
+import "./assets/Styles/app.css";
 
 function App() {
   return (
     <div className="wrapper">
-      <Logo />
-      <Title />
-      <Subtitle />
-      <GetNotifiedForm />
-      <HeroImage />
+      <section className="site-info">
+        <Logo />
+        <Title />
+        <Subtitle />
+        <GetNotifiedForm />
+      </section>
+      <section className="heroImage">
+        <HeroImage />
+      </section>
     </div>
   );
 }
