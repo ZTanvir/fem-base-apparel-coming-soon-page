324dafd5aa72e1d934e6513c08a628cb5efc48f9
ztanvir <zahirulopel@gmail.com>
2023-11-30 20:55:45 +0600

    background color added for submit button in notified component

--- src/Component/GetNotifiedForm.jsx
+++ src/Component/GetNotifiedForm.jsx
@@ -1,9 +1,11 @@
 import buttonImg from "../assets/images/icon-arrow.svg";
+import notifiedFormStyle from "../assets/Styles/notifiedForm.module.css";
+
 const GetNotifiedForm = () => {
   const handleSubmit = () => {};
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Email Address" />
+    <form className={notifiedFormStyle.formStyle} onSubmit={handleSubmit}>
+      <input type="text" placeholder="Email Address" required />
       <button type="submit">
         <img src={buttonImg} alt="right arrow" />
       </button>
