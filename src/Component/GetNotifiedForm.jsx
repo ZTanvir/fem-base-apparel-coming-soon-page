376ebf3b44ca5eef1bed6cc2a80b8f784b4b7825
ztanvir <zahirulopel@gmail.com>
2023-12-14 20:26:50 +0600

    Error message appear when email is invalid

--- src/Component/GetNotifiedForm.jsx
+++ src/Component/GetNotifiedForm.jsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
 import buttonImg from "../assets/images/icon-arrow.svg";
 import notifiedFormStyle from "../assets/Styles/notifiedForm.module.css";
 
 const GetNotifiedForm = () => {
-  const handleSubmit = () => {};
+  const [email, setEmail] = useState("");
+  const [isError, setIsError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const isEmailValid = (email) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (isEmailValid(email)) {
+      setEmail("");
+      setErrorMsg("");
+    } else {
+      setIsError(true);
+      setErrorMsg("Please provide a valid email");
+    }
+  };
   return (
-    <form className={notifiedFormStyle.formStyle} onSubmit={handleSubmit}>
-      <input type="text" placeholder="Email Address" required />
-      <button type="submit">
-        <img src={buttonImg} alt="right arrow" />
-      </button>
-    </form>
+    <div>
+      <form className={notifiedFormStyle.formStyle} onSubmit={handleSubmit}>
+        <input
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            setErrorMsg("");
+          }}
+          placeholder="Email Address"
+        />
+        <span className={notifiedFormStyle.errorMsg}>
+          {isError ? errorMsg : ""}
+        </span>
+
+        <button type="submit">
+          <img src={buttonImg} alt="right arrow" />
+        </button>
+      </form>
+    </div>
   );
 };
 export default GetNotifiedForm;
