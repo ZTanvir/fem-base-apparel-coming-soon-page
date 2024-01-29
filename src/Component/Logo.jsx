c3730d235832badf5d238d6ed1a2f0d69806a3a5
ztanvir <zahirulopel@gmail.com>
2024-01-29 18:16:19 +0600

    Add padding in the logo section for mobile screen

--- src/Component/Logo.jsx
+++ src/Component/Logo.jsx
@@ -1,11 +1,15 @@
 import LogoImg from "../assets/images/logo.svg";
+import logoSectionStyle from "../assets/Styles/logoSection.module.css";
+
 const Logo = () => {
   return (
-    <img
-      className="logo-img"
-      src={LogoImg}
-      alt="A logo which is looks like a pink ring and name is BASE APPAREL"
-    />
+    <div className={logoSectionStyle.logoSection}>
+      <img
+        className="logo-img"
+        src={LogoImg}
+        alt="A logo which is looks like a pink ring and name is BASE APPAREL"
+      />
+    </div>
   );
 };
 export default Logo;
